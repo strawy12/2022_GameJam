0cec2aef506a291c4b6fc045c2290573d1980305
435yj <yujin050420@naver.com>
2022-07-11 15:01:40 +0900

    카메라 드래그

--- Assets/02.Scripts/Utils/GameManager.cs
+++ Assets/02.Scripts/Utils/GameManager.cs
@@ -6,4 +6,44 @@ using static Define;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+    [SerializeField] private float _cameraSpeed = 5f;
+    [SerializeField] private float _duration = 0f;
+
+    [SerializeField] private Transform _cameraTransform;
+        
+    private float _currentDir = 0f;
+
+    public void CameraMove(float dir)
+    {
+        if (dir > 0)
+        {
+            _cameraTransform.Translate(Vector3.right * Time.deltaTime * _cameraSpeed);
+            _currentDir = 1;
+        }
+        else if (dir < 0)
+        {
+            _cameraTransform.Translate(Vector3.left * Time.deltaTime * _cameraSpeed);
+            _currentDir = -1;
+        }
+        else
+        {
+            StartCoroutine(ReturnCoroutine());
+        }
+    }
+
+    private IEnumerator ReturnCoroutine()
+    {
+        float time = _duration;
+        float moveSpeed = _cameraSpeed;
+
+        while (time > 0)
+        {
+            _cameraTransform.Translate(Vector3.right * _currentDir * Time.deltaTime * moveSpeed);
+            moveSpeed = Mathf.Lerp(0f, moveSpeed, time / _duration);
+            yield return null;
+            time -= Time.deltaTime;
+
+        }
+    }
+
 }
