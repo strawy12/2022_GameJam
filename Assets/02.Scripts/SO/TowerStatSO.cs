293e031c92a3e6d0b28f4f1a9f0e45639f862e8b
injun2005 <77712212+injun2005@users.noreply.github.com>
2022-07-11 14:03:44 +0900

    tower.cs 기본

---
+++ Assets/02.Scripts/SO/TowerStatSO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ETowerType
+{
+    ActiveType,
+    PassiveType,
+    FixingType
+}
+
+[CreateAssetMenu(menuName = "SO/Tower/Stats")]
+public class TowerStatSO : ScriptableObject
+{
+    public int damage = 1;
+    [Range(1, 5)]public float weight = 1;
+
+    public ETowerType towerType = ETowerType.PassiveType;
+}
---
+++ Assets/02.Scripts/Tower/Tower.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : PoolableMono
+{
+    #region TowerData
+    [SerializeField] protected TowerStatSO _towerStatData;
+
+    public int Damage
+    {
+        get
+        {
+            return _towerStatData.damage;
+        }
+    }
+
+    public float Weight
+    {
+        get
+        {
+            return _towerStatData.weight;
+        }
+    }
+
+    public ETowerType TowerType 
+    { 
+        get
+        {
+            return _towerStatData.towerType;
+        }
+    }
+    #endregion
+
+    protected bool isStop = false;
+    protected Transform baseTrm;
+    protected Rigidbody2D _rigidbody;
+    protected virtual void Awake()
+    {
+        baseTrm = transform.Find("baseTransform");
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public override void Reset()
+    {
+        isStop = false;
+        _rigidbody.constraints = 0;
+    }
+
+    public virtual void DestroyTower()
+    {
+        PoolManager.Instance.Push(this);
+    }
+
+    public virtual void StartThrowed()
+    {
+
+    }
+
+    public virtual void UseSkill()
+    {
+        
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground")) 
+        {
+            isStop = true;
+            _rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        else if(collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            
+        }
+    }
+}
